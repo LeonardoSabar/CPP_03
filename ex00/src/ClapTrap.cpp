b5d4dfbe762bc245827b967a1e0a9630123098b0
Leonardo Barbosa Sales <leonardosabar@gmail.com>
2025-02-03 21:42:57 -0300

    00 IS DONE!

--- ex00/src/ClapTrap.cpp
+++ ex00/src/ClapTrap.cpp
@@ -2,27 +2,77 @@
 
 ClapTrap::ClapTrap(std::string name) : Name(name), HitPoints(10), EnergyPoints(10), AttackDamage(0)
 {
-    std::cout <<  Y << "ClapTrap " << Name << " created!" << RST << std::endl;
+    if (HitPoints > 100 || EnergyPoints > 100 || AttackDamage > 100)
+    {
+        std::cout << O << "Invalid points for ClapTrap " << Name << "!" << RST << std::endl;
+        return;
+    }
+    std::cout << Y << "ClapTrap " << Name << " created!" << RST << std::endl;
 }
+
 
 void ClapTrap::attack(const std::string &target)
 {
-    std::cout << RED << "ClapTrap " << Name << " attacks " << target << ", causing " << AttackDamage << " points of damage!" << RST << std::endl;
+    std::cout << P << "ClapTrap " << Name << " attacks " << target 
+              << ", causing " << AttackDamage << " points of damage!" << RST << std::endl;
 }
+
 
 void ClapTrap::takeDamage(unsigned int amount)
 {
+    if (amount > INT_MAX)
+    {
+        std::cout << RED << "Invalid damage amount!" << RST << std::endl;
+        return;
+    }
+
+    if (HitPoints == 0)
+    {
+        std::cout << M << "ClapTrap " << Name << " is already dead!" << RST << std::endl;
+        return;
+    }
+
+    if (amount >= HitPoints)
+    {
+        std::cout << RED << "ClapTrap " << Name << " received " << amount 
+            << " points of damage and died!" << RST << std::endl;
+        HitPoints = 0;
+        return;
+    }
+
     HitPoints -= amount;
-    std::cout << G << Name << " took " << amount << " points of damage!" << RST << std::endl;
+    std::cout << RED << "ClapTrap " << Name << " took " 
+        << amount << " points of damage!" << RST << std::endl;
 }
+
 
 void ClapTrap::beRepaired(unsigned int amount)
 {
+
+    if (amount > INT_MAX)
+    {
+        std::cout << M << "Invalid repair amount!" << RST << std::endl;
+        return;
+    }
+
+    if (HitPoints <= 0)
+    {
+        std::cout << M << "ClapTrap " << Name 
+            << " cannot be repaired, because it is dead!" << RST << std::endl;
+        return;
+    }
+
     HitPoints += amount;
-    std::cout << B << Name << " was repaired for " << amount << " hit points!" << RST << std::endl;
+    if (HitPoints > 10)
+    {
+        HitPoints = 10;
+    }
+
+    std::cout << G << "ClapTrap " << Name << " was repaired for " << amount 
+        << " hit points. And now it has " << HitPoints << " hit points!" << RST << std::endl;
 }
 
-ClapTrap::~ClapTrap()
-{
+
+ClapTrap::~ClapTrap() {
     std::cout << O << "ClapTrap " << Name << " destroyed!" << RST << std::endl;
 }
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -3,8 +3,9 @@
 int main()
 {
     ClapTrap clap("Swordfish");
-    clap.attack("Enemy");
-    clap.takeDamage(3);
+    clap.attack("Pirate");
+    clap.takeDamage(1);
+    clap.takeDamage(5);
     clap.beRepaired(2);
 
     return 0;
